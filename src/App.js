19a88ab9b439dd3ce5bf4328ec80800c3d5a80b5
SaulReznik <robhov97@gmail.com>
2021-04-25 18:28:56 +0400

    rotation achived

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import useStyles from './styles.js';
 import Slider from './components/Slider';
 import Screen from './components/Screen';
 
+import { degreeToRadian, radianToDegree } from './utils/helpers';
+
 const App = () => {
   const [matrix, setMatrix] = useState({
     scaleX: 1,
@@ -13,6 +15,7 @@ const App = () => {
     translateX: 0,
     translateY: 0
   });
+  const [rotationAngle, setRotationAngle] = useState(0);
 
   const classes = useStyles();
   const { app } = classes;
@@ -20,9 +23,34 @@ const App = () => {
   const callback = (type, value) => { setMatrix((prevMetrix) => ({...prevMetrix, [type]: value })) };
   const sliderHandler = useCallback(callback, []);
 
+  const rotate = e => {
+    const { value } = e.target;
+    const rotationRadian = degreeToRadian(value);
+    const newScale = Math.cos(rotationRadian);
+    const newSkewX = -Math.sin(rotationRadian);
+    const newSkewY = Math.sin(rotationRadian);
+    setRotationAngle(value);
+    setMatrix({
+      ...matrix,
+      scaleX: newScale,
+      scaleY: newScale,
+      skewX: newSkewX,
+      skewY: newSkewY
+    });
+  };
+
   return (
     <div className={app}>
       <div className="sliderBar">
+        <div>
+          <div>Rotation Angle: {rotationAngle}</div>
+          <input
+            type="range"
+            max="360"
+            value={rotationAngle}
+            onChange={rotate}
+          />
+        </div>
         {
           Object.keys(matrix).map(key => {
             return (
---
+++ src/utils/helpers.js
@@ -0,0 +1,2 @@
+export const degreeToRadian = degree => degree * (Math.PI / 180);
+export const radianToDegree = radian => radian * (180 / Math.PI);
