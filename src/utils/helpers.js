3b916d7834b3411e1354201a79af6a4f1bec872d
SaulReznik <robert.hovhannisyan@picsart.com>
2021-05-15 21:45:19 +0400

    some helper functions ready

---
+++ src/utils/helpers.js
@@ -0,0 +1,52 @@
+export const degreeToRadian = degree => degree * (Math.PI / 180);
+export const radianToDegree = radian => radian * (180 / Math.PI);
+
+export const multiplyMatixWithPoint = (matrix, point) => {
+  return point.map((dimension, index) => {
+    let result = 0;
+
+    for (let i = 0; i < 4; i++) {
+      const matrixIndex = index * 4 + i;
+      result += dimension * matrix[matrixIndex];
+    }
+
+    return result;
+  })
+};
+
+export const matrixToPoints = matrix => {
+  const result = [];
+
+  for (let i = 0; i < 4; i++) {
+    const onePoint = [];
+
+    for (let j = 0; j < 4; j++) {
+      onePoint.push(matrix[i + j]);
+    }
+
+    result.push(onePoint);
+  }
+
+  return result;
+};
+
+export const pointsToMatrix = points => points.reduce((acc, points) => [...acc, ...points], []);
+
+export const multiplyMatrices = (matrixA, matrixB) => {
+  const separatePoints = matrixToPoints(matrixB);
+
+  return pointsToMatrix(separatePoints.map(point => multiplyMatixWithPoint(matrixA, point)));
+};
+
+export const matrixToCSSMatrix = matrix => `matrix3d(${matrix.join(',')})`;
+
+export const rotationMatrixGenerator = angle => {
+  const radians = degreeToRadian(angle);
+
+  return [
+    Math.cos(radians), -Math.sin(radians),    0,    0,
+    Math.sin(radians),  Math.cos(radians),    0,    0,
+         0,       0,    1,    0,
+         0,       0,    0,    1
+  ];
+}
